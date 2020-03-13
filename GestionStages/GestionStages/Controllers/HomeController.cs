e64cfdced112e42333bab6286d4e2f504c9d5161
Zachary Alexandre <zachary.alexandre430@gmail.com>
2020-03-13 13:12:54 -0400

    Ajout repo

--- GestionStages/GestionStages/Controllers/HomeController.cs
+++ GestionStages/GestionStages/Controllers/HomeController.cs
@@ -11,9 +11,7 @@ namespace GestionStages.Controllers
 {
     public class HomeController : Controller
     {
-        public static SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=GestionStage;Integrated Security=True");
-        private SqlDataReader dr;
-        private SqlCommand sql;
+        Repositories.repoMSSQL repo = new Repositories.repoMSSQL();
         public IActionResult Index()
         {
             return View();
@@ -24,81 +22,17 @@ namespace GestionStages.Controllers
             return View();
         }
 
-        public IActionResult Test()
+        public IActionResult Test(int id)
         {
-            List<Etudiant> lesEtudiants = new List<Etudiant>();
-            sql = new SqlCommand();
-            try
-            {
-                conn.Open();
-                sql.Connection = conn;
-                sql.CommandText = "EXEC pGetAllActiveEtudiant";
-                dr = sql.ExecuteReader();
-                while (dr.Read())
-                {
-                    Etudiant etudiant = new Etudiant();
-                    etudiant.IDEtudiant = (int)dr.GetValue(0);
-                    etudiant.Programme = (string)dr.GetValue(1);
-                    etudiant.NoDA = (int)dr.GetValue(2);
-                    etudiant.Prenom = (string)dr.GetValue(3);
-                    etudiant.Nom = (string)dr.GetValue(4);
-                    etudiant.Courriel = (string)dr.GetValue(5);
-                    lesEtudiants.Add(etudiant);
-                }
-                ViewBag.lesEtudiants = lesEtudiants;
-                return View();
-            }
-            catch
-            {
-                ViewBag.lesEtudiants = lesEtudiants;
-                return Error();
-            }
-            finally
-            {
-                conn.Close();
-            }
+            ViewBag.id = id;
+            ViewBag.lesEtudiants = repo.getAllEtudiants();
+            return View();
         }
 
         public IActionResult ListeMilieuStage()
         {
-            List<MilieuStage> lesMilieus = new List<MilieuStage>();
-            sql = new SqlCommand();
-            try
-            {
-                conn.Open();
-                sql.Connection = conn;
-                sql.CommandText = "EXEC pGetAllMilieuStage";
-                dr = sql.ExecuteReader();
-                while (dr.Read())
-                {
-                    MilieuStage milieu = new MilieuStage();
-                    milieu.IDMilieuStage = (int)dr.GetValue(0);
-                    milieu.Titre = (string)dr.GetValue(1);
-                    milieu.Description = (string)dr.GetValue(2);
-                    milieu.NoCivique = (string)dr.GetValue(3);
-                    milieu.Rue = (string)dr.GetValue(4);
-                    milieu.CodePostal = (string)dr.GetValue(5);
-                    milieu.Ville = (string)dr.GetValue(6);
-                    milieu.Province = (string)dr.GetValue(7);
-                    milieu.Pays = (string)dr.GetValue(8);
-                    milieu.NoTelephone = (string)dr.GetValue(9);
-                    milieu.Etat = (bool)dr.GetValue(10);
-                    lesMilieus.Add(milieu);
-                }
-                ViewBag.lesMilieus = lesMilieus;
-                ViewBag.error = "";
-                return View();
-            }
-            catch(Exception e)
-            {
-                ViewBag.lesMilieus = lesMilieus;
-                ViewBag.error = e.Message;
-                return Error();
-            }
-            finally
-            {
-                conn.Close();
-            }
+            ViewBag.lesMilieus = repo.getAllMilieuStage();
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ GestionStages/GestionStages/Repositories/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestionStages.Models;
+
+namespace GestionStages.Repositories
+{
+    public abstract class Repository
+    {
+        public abstract List<MilieuStage> getAllMilieuStage();
+        public abstract List<Etudiant> getAllEtudiants();
+    }
+}
