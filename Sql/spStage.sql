c141fce31f511d4d20e8ade913837519e7d363a9
Johnatan Fortier-Roy <djohnat_13@hotmail.com>
2020-03-13 12:25:19 -0400

    Script stored procedure

--- Sql/spStage.sql
+++ Sql/spStage.sql
@@ -7,11 +7,29 @@ SELECT [IDStage], [IDMilieuStage], [Titre], [Description], [Statut], [NbPostes],
 FROM Stage
 GO
 
-CREATE PROC pAddSetStage(@IDMilieuStage_IN INT, @Titre_IN Varchar(100),@Description_IN Varchar(1000),
+CREATE PROC pAddSetCreateStage(@IDMilieuStage_IN INT, @Titre_IN Varchar(100),@Description_IN Varchar(1000),
 @NbPoste_IN INT, @Statut_IN TINYINT, @PeriodeTravail_IN TINYINT, @NbHeureSemaine_IN INT, @DateDebut_IN DateTime, @DateFin_IN DateTime, @Etat_IN Bit)
 AS
 BEGIN
 INSERT INTO Stage ([IDMilieuStage], [Titre], [Description], [NbPostes], [Statut], [PeriodeTravail], [NbHeureSemaine], [DateDebut], [DateFin], [Etat], [DateHeureCreation], [DateHeureModification])
 VALUES (@IDMilieuStage_IN, @Titre_IN, @Description_IN, @NbPoste_IN, @Statut_IN, @PeriodeTravail_IN, @NbHeureSemaine_IN, @DateDebut_IN, @DateFin_IN, @Etat_IN, GETDATE(), null)
 END
+GO
+
+CREATE PROC pAddSetUpdateStage(@IDStage_IN INT, @IDMilieuStage_IN INT, @Titre_IN Varchar(100),@Description_IN Varchar(1000),
+@NbPoste_IN INT, @Statut_IN TINYINT, @PeriodeTravail_IN TINYINT, @NbHeureSemaine_IN INT, @DateDebut_IN DateTime, @DateFin_IN DateTime, @Etat_IN Bit, @DateHeureCreation DateTime)
+AS
+BEGIN
+UPDATE Stage ([IDMilieuStage], [Titre], [Description], [NbPostes], [Statut], [PeriodeTravail], [NbHeureSemaine], [DateDebut], [DateFin], [Etat], [DateHeureCreation], [DateHeureModification])
+SET [IDMilieuStage]= @IDMilieuStage_IN, [Titre] = @Titre_IN, [Description] = @Description_IN, [NbPostes] = @NbPoste_IN, [Statut] = @Statut_IN, [PeriodeTravail] = @PeriodeTravail_IN, [NbHeureSemaine] = @NbHeureSemaine_IN,
+[DateDebut] = @DateDebut_IN, [DateFin] = @DateFin_IN,[Etat] = @Etat_IN, [DateHeureCreation] = @DateHeureCreation, [DateHeureModification] = GETDATE() 
+WHERE [IDStage] = @IDStage_IN
+END
+GO
+
+CREATE PROC pGetStageByID(@IDStage_IN INT)
+AS
+SELECT [IDMilieuStage], [Titre], [Description], [Statut], [NbPostes], [PeriodeTravail], [NbHeureSemaine], [DateDebut], [DateFin], [Etat]
+FROM Stage
+WHERE [IDStage] = @IDStage_IN;
 GO
